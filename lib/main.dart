115084351e0c246421869705cf38998f95cd8087
mdshadatrahman <shadat.rahman.464@gmail.com>
2023-02-18 20:15:46 +0600

    structure

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String emoji;
+
+  const Person({
+    required this.name,
+    required this.age,
+    required this.emoji,
+  });
+}
+
+const people = [
+  Person(name: 'John', age: 20, emoji: '👨'),
+  Person(name: 'Jane', age: 21, emoji: '👩'),
+  Person(name: 'Jack', age: 22, emoji: '👦'),
+];
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -28,10 +47,74 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Home Page'),
+        title: const Text('People'),
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final person = people[index];
+          return ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DetailsPage(person: person),
+                ),
+              );
+            },
+            leading: Text(
+              person.emoji,
+              style: const TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            title: Text(person.name),
+            subtitle: Text('${person.age} years old'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({
+    super.key,
+    required this.person,
+  });
+  final Person person;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          person.emoji,
+          style: const TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              person.name,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              '${person.age} years old',
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
